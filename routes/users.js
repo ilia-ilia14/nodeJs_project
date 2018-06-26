021f096c3e549a39190c0ae472bd1430af693b9e
ilia-ilia14 <simon.ilia.sd@gmail.com>
2018-06-26 15:52:44 -0600

    -

--- routes/users.js
+++ routes/users.js
@@ -6,7 +6,7 @@ var express = require('express');
 var router = express.Router();
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
-var io = require('socket.io').listen(4000).sockets;
+var io = require('socket.io').listen('https://afternoon-ocean-62570.herokuapp.com/').sockets;
 
 
 var User = require('../models/user');
